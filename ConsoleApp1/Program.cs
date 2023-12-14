b645b8a70cd08d53e9f8968028f8872425e04bd8
Bisam Paudel <bizzen78@gmail.com>
2023-12-14 09:26:36 +0545

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Circle
+{
+    public readonly double PI;
+    public readonly double Radius;
+
+    public Circle(double radius)
+    {
+        PI = 3.14159; // Initializing readonly variable inside constructor
+        Radius = radius; // Another readonly variable initialization inside constructor
+    }
+
+    public double CalculateArea()
+    {
+        return PI * Radius * Radius;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Circle c = new Circle(5);
+        Console.WriteLine($"Area of the circle with radius 5 is: {c.CalculateArea()}");
+
+        // The following line would produce a compile error since PI is readonly
+        // c.PI = 3.14; 
+    }
+}
